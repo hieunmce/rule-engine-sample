f3802a4636ac41332053cae05746931e9e085381
hieunmce <hieunmce@gmail.com>
2020-02-28 00:34:23 +0700

    correct handler sqs event

--- main.go
+++ main.go
@@ -1,10 +1,34 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"log"
+
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/hieunmce/rule-engine-sample/core"
 	"github.com/hieunmce/rule-engine-sample/handler"
 )
 
+func SQSHandler(ctx context.Context, sqsEvent events.SQSEvent) error {
+	for _, message := range sqsEvent.Records {
+		var pt core.Patient
+		err := json.Unmarshal([]byte(message.Body), &pt)
+		if err != nil {
+			log.Println("failed to parse sqsEvent", err)
+			continue
+		}
+
+		err = handler.HandleEnqueue(&pt)
+		if err != nil {
+			log.Println("failed to handle sqsEvent", err)
+		}
+	}
+
+	return nil
+}
+
 func main() {
-	lambda.Start(handler.HandleEnqueue)
+	lambda.Start(SQSHandler)
 }
